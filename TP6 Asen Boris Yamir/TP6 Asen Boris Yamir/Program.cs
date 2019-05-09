60afe1b574ca1fac82fd017982758a850d2db306
Claudio <Claudioelreloco@gmail.com>
2019-05-09 19:02:51 -0300

    Primer Proyecto en VS

---
+++ TP6 Asen Boris Yamir/TP6 Asen Boris Yamir/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP6_Asen_Boris_Yamir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int vble;
+            vble = int.Parse(Console.ReadLine());
+            Console.Write(vble);
+
+            if (vble < 50)
+            {
+                Console.Write(" es menor de 50");
+            }
+            else
+            {
+                Console.Write(" es mayor de 50");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
